f09a96d3e46f0ccd1eb4987a222cbf9d7ae0d3a3
Shrey <115035182+Shreyyyyy@users.noreply.github.com>
2024-08-31 09:31:38 +0530

    index.jsx

--- pages/work/index.jsx
+++ pages/work/index.jsx
@@ -29,8 +29,22 @@ const Work = () => {
               exit="hidden"
               className="mb-4 max-w-[400px] mx-auto lg:mx-0"
             >
-             Shreyans Jain has led and delivered a range of impactful technology projects across diverse domains. From enhancing operational efficiencies with robust full-stack development to pioneering predictive models in safety applications, his projects emphasize innovation and practicality. His portfolio includes advanced applications in inventory management, threat detection, facial recognition, and user engagement platforms.
-
+              I have led and delivered a range of impactful technology projects across diverse domains. My work emphasizes innovation and practicality, focusing on enhancing operational efficiencies through robust full-stack development and pioneering predictive models in safety applications.
+              
+              My portfolio includes advanced applications in:
+              
+              Restaurant Recommendation using Neural Networks
+              Developing a system that utilizes machine learning algorithms to provide personalized restaurant recommendations based on user preferences and past dining experiences.
+              
+              Hospital Length of Stay Prediction
+              Creating a predictive model that forecasts the expected length of stay for patients in a hospital setting, enabling better resource allocation and improved patient outcomes.
+              
+              Automated Attendance Monitoring
+              Implementing a system that automates the process of tracking employee attendance, reducing manual effort and ensuring accurate record-keeping.
+              
+              Peddle Mobile App
+              Designing and developing a mobile application that promotes sustainable transportation by providing users with a platform to rent bicycles and track their rides, contributing to a greener environment.
+              These projects reflect my commitment to using technology to solve real-world problems and create meaningful impacts.
             </motion.p>
           </div>
 
